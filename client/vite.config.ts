b379112f3bdd938281d858fbc1a05d893f8853f6
Danne56 <deffa812@gmail.com>
2025-06-16 21:55:41 +0700

    refactor: update Vite configuration to load environment variables dynamically

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: ["5173.servy.my.id"]
-  },
-  resolve: {
-    alias: {
-      "@client": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../server/src"),
-      "@shared": path.resolve(__dirname, "../shared/src")
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      allowedHosts: env.VITE_ALLOWED_HOSTS?.split(',').map(host => host.trim()) || ['localhost:5173'],
+    },
+    resolve: {
+      alias: {
+        "@client": path.resolve(__dirname, "./src"),
+        "@server": path.resolve(__dirname, "../server/src"),
+        "@shared": path.resolve(__dirname, "../shared/src")
+      }
     }
   }
 })
